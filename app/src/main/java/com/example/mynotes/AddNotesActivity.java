0a974c6bc281ea054fe082114cfa597519b7b398
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2020-11-26 16:47:32 +0530

    Part-4

--- app/src/main/java/com/example/mynotes/AddNotesActivity.java
+++ app/src/main/java/com/example/mynotes/AddNotesActivity.java
@@ -3,12 +3,41 @@ package com.example.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddNotesActivity extends AppCompatActivity {
+
+    EditText title, description;
+    Button addNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_notes);
+
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
+        addNote = findViewById(R.id.addNote);
+
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (!TextUtils.isEmpty(title.getText().toString()) && !TextUtils.isEmpty(description.getText().toString())) {
+                    DatabaseClass db = new DatabaseClass(AddNotesActivity.this);
+                    db.addNotes(title.getText().toString(), description.getText().toString());
+                } else {
+                    Toast.makeText(AddNotesActivity.this, "Both Fields Required", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/mynotes/DatabaseClass.java
@@ -0,0 +1,67 @@
+package com.example.mynotes;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseClass extends SQLiteOpenHelper {
+
+    Context context;
+    private static final String DatabaseName="MyNotes";
+    private static final int DatabaseVersion=1;
+
+    private static final String TableName="mynotes";
+    private static final String ColumnId="id";
+    private static final String ColumnTitle="title";
+    private static final String ColumnDescription="description";
+
+
+    public DatabaseClass(@Nullable Context context) {
+        super(context, DatabaseName, null, DatabaseVersion);
+        this.context=context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+            String query= "CREATE TABLE " + TableName +
+                    " (" + ColumnId + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    ColumnTitle + " TEXT, " +
+                    ColumnDescription + " TEXT);";
+
+            db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TableName);
+        onCreate(db);
+    }
+
+    void addNotes(String title, String description)
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+
+        ContentValues cv=new ContentValues();
+
+        cv.put(ColumnTitle,title);
+        cv.put(ColumnDescription,description);
+
+        long resultValue = db.insert(TableName,null,cv);
+
+        if (resultValue ==-1)
+        {
+            Toast.makeText(context, "Data Not Added", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(context, "Data Added Successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
